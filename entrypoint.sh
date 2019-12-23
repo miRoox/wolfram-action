22cb95fcf9b11c2f8704f42dffa3cfc1aa9bab46
miroox <miroox@outlook.com>
2019-12-23 20:26:22 +0800

    action entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,24 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+set -e
 
+error() {
+  echo "::error :: $1"
+  exit 1
+}
+
+script_file="$1"
+
+if [ -z "$script_file" ]; then
+  error "Input 'script-file' is missing."
+fi
+
+if [ -z "$WOLFRAM_ID" ]; then
+  error "Environment vaiable WOLFRAM_ID is missing."
+fi
+
+if [ -z "$WOLFRAM_PASSWORD" ]; then
+  error "Environment vaiable WOLFRAM_PASSWORD is missing."
+fi
+
+/usr/bin/wolframscript -username $WOLFRAM_ID -password $WOLFRAM_PASSWORD -file $script_file
