2e5b93481939bf6218d676e2f312b4e2b356a03f
miroox <miroox@outlook.com>
2019-12-24 23:06:14 +0800

    fix quote

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ if [ -z "$WOLFRAM_PASSWORD" ]; then
   error "Environment vaiable WOLFRAM_PASSWORD is missing."
 fi
 
-echo '$WOLFRAM_ID
-$WOLFRAM_PASSWORD' | /usr/bin/wolframscript
+echo "$WOLFRAM_ID
+$WOLFRAM_PASSWORD" | /usr/bin/wolframscript
 
 /usr/bin/wolframscript -file $script_file
